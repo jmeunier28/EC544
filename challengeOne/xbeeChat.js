3312f3ccfc721688c4949ff1ddd63fc4621f3198
= <jmeunier@bu.edu>
2016-09-20 11:07:05 -0400

    final version

--- challengeOne/xbeeChat.js
+++ challengeOne/xbeeChat.js
@@ -35,6 +35,12 @@ http.listen(3000, function(){
 
 
 
+var info_object = function(){
+  this.id;
+  this.info = [];
+}
+
+
 sp.on("open", function () {
   console.log('open');
 
@@ -42,16 +48,57 @@ sp.on("open", function () {
   var names = []; //names will be assigned to each arduino 
   var countArray = [0,0,0,0];
   var times = [];
+  var sensor_array = [];
+  var flag= false;
 
   sp.on('data', function(data) {
     var time = new Date();
 
     times.push(time.getTime()); //add to time array
     console.log(data); // log incoming data from XBee
-    message = data.split(','); //separate data
-    console.log(message);
 
-   if(message[0]=='A'){
+  //setInterval(function(){
+    
+    message = data.split(','); //separate data
+
+    if (sensor_array.length == 0){
+
+        var whatup = new info_object();
+        whatup.id = message[0];
+        whatup.info.push(message[1]);
+        console.log(message[0]);
+        sensor_array.push(whatup);
+        console.log("Creating initial ID");
+
+    }
+
+    for(var i=0; i<sensor_array.length; i++){
+
+      if(sensor_array[i].id== String(message[0])){
+        console.log(sensor_array[i].id);
+        sensor_array[i].info.push(message[1]);
+        flag = true;
+        console.log("I already have " + sensor_array[i].id + " and I'm adding to it");
+      }
+    }
+
+    if (flag==false){
+        console.log("Creating new id object...");
+        var heythere = new info_object();
+        heythere.id = message[0];
+        heythere.info.push(message[1]);
+        sensor_array.push(heythere);
+      }
+    
+    
+    for (var i = 0; i < sensor_array.length-1;i++){
+      names.push(sensor_array[i]['id']);
+      tempArray[i] = sensor_array[i]['info'].pop();
+
+    }
+    // console.log("This is temp array " + tempArray);
+
+   /*if(message[0]=='A'){
 
       names.push(message[0]);
       tempArray[0]= parseInt(message[1]);
@@ -82,41 +129,42 @@ sp.on("open", function () {
       tempArray[3] = parseInt(message[1]);
       countArray[3]++;
 
-   }
+   }*/
 
    // get all 4 entries and sum them together after the array 
-   // of times has 4 entries 
-
-  //var timeDiff = times[times.length - 1] - times[0]; 
-  if (times.length == 4){
+   // of times has 4 entries   
 
     var counter = 0;
     var sum = 0;
+    console.log("temp array is "+ tempArray);
 
-    for (var i = 0; i < tempArray.length;i++){
+
+      for (var i = 0; i < tempArray.length;i++){
+      console.log("im computing the average");
       sum += tempArray[i]; //add each temperature together 
-      counter++;
+      counter++; }
+
+      var average = (sum/counter).toFixed(2);
+      console.log("Data reveived from " + names + ", and the average temperature is "+ average +"*C. ");
+      var avgString = "Data reveived from " + names + ", and the average temperature is "+ average +"*C";
+      io.emit(avgString); 
+
+      //tempArray = [0,0,0,0];
+
+    for (var i =0; i < tempArray.length; i++){
+      console.log("Individual Sensor " + names[i] + "Value: " + tempArray[i]);
+      var individualTemp = "Individual Sensor " + names[i] + "Value: " + tempArray[i];
+      io.emit(individualTemp);
     }
-  }
 
-  var avg = (sum/counter).toFixed(2);
-  console.log("Data reveived from " + names + ", and the average temperature is "+ average +"*C. ");
-  var avgString = "Data reveived from " + names + ", and the average temperature is "+ average +"*C";
-  io.emit(avgString);
-
-  for (var i =0; i < tempArray. length; i++){
-    console.log("Individual Sensor " + names[i] + "Value: " + tempArray[i]);
-    var individualTemp = "Individual Sensor " + names[i] + "Value: " + tempArray[i];
-    io.emit(individualTemp)
-  }
-
-  for (var i = 0; i < countArray.length;i++){
-    if (countArray[i]== 0 ){
-      console.log("Error Sending Data From Sensor Number " + i);
-      var errmsg = "Error Sending Data From Sensor Number " + i;
-      io.emit(errmsg);
+    for (var i = 0; i < countArray.length;i++){
+      if (countArray[i]== 0 ){
+        console.log("Error Sending Data From Sensor Number " + i);
+        var errmsg = "Error Sending Data From Sensor Number " + i;
+        io.emit(errmsg);
+      }
     }
-  }
+
+    //}, 2000);
   });
 });
-
