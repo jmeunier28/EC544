ac687a0417bccab4164252f765cee335e8c6a79f
= <jmeunier@bu.edu>
2016-09-27 11:17:48 -0400

    new scalable code added in comments

--- challengeTwo/xbeeChat.js
+++ challengeTwo/xbeeChat.js
@@ -33,23 +33,69 @@ http.listen(3000, function(){
   console.log('listening on *:3000');
 });
 
-
+//code purposely commented out will add back later
+/*var info_object = function(){
+  this.id;
+  this.info = [];
+}*/
 
 sp.on("open", function () {
   console.log('open');
 
   var tempArray = [0,0,0,0];
+  //var tempArray = [];
   var names = []; //names will be assigned to each arduino 
   var countArray = [0,0,0,0];
+  //var countArray = [];
   var times = [];
 
   sp.on('data', function(data) {
+    var flag = false;
     var time = new Date();
 
     times.push(time.getTime()); //add to time array
     //console.log(data); // log incoming data from XBee
     message = data.split(','); //separate data
     //console.log(message);
+
+    //code purposely commented out... would give ability to scale to many devices
+
+    /*if (sensor_array.length == 0){
+
+        var whatup = new info_object();
+        whatup.id = message[0];
+        whatup.info.push(message[1]);
+        console.log(message[0]);
+        sensor_array.push(whatup);
+        console.log("Creating initial ID");
+
+    }
+
+    for(var i=0; i<sensor_array.length; i++){
+
+      if(sensor_array[i].id== String(message[0])){
+        console.log(sensor_array[i].id);
+        sensor_array[i].info.push(message[1]);
+        flag = true;
+        console.log("I already have " + sensor_array[i].id + " and I'm adding to it");
+      }
+    }
+
+    if (flag==false){
+        console.log("Creating new id object...");
+        var heythere = new info_object();
+        heythere.id = message[0];
+        heythere.info.push(message[1]);
+        sensor_array.push(heythere);
+      }
+    
+    
+    for (var i = 0; i < sensor_array.length-1;i++){
+      names.push(sensor_array[i]['id']);
+      tempArray.push(sensor_array[i]['info'].pop());
+    }
+    console.log("This is temp array " + tempArray);*/
+
 
    if(message[0]=='A'){
 
@@ -126,13 +172,6 @@ var calc_average = function(){
   setInterval(function(){ calc_average() },2000);
 
 
-
-  /*for (var i =0; i < tempArray. length; i++){
-    console.log("Individual Sensor " + names[i] + "Value: " + tempArray[i]);
-    var individualTemp = "Individual Sensor " + names[i] + "Value: " + tempArray[i];
-    io.emit('chat message',individualTemp)
-  }*/
-
   for (var i = 0; i < countArray.length;i++){
     if (countArray[i]== 0 ){
       console.log("Error Sending Data From Sensor Number " + i);
@@ -145,5 +184,25 @@ var calc_average = function(){
     error = true;
     console.log("Adjusting Average...");
   }
+    //code purposely commented out... calc temp avg of device ids that have been added
+    //dynamically... not functioning at this time
+    /*var counter = 0;
+    var sum = 0;
+    var calc_avg = function(){
+    //console.log("temp array is "+ tempArray);
+
+
+      for (var i = 0; i < tempArray.length;i++){
+      console.log("im computing the average");
+      sum += tempArray[i]; //add each temperature together 
+      console.log("this is the sum" + sum);
+      counter++; }
+
+      var average = (sum/counter).toFixed(2);
+      console.log("Data reveived from " + names + ", and the average temperature is "+ average +"*C. ");
+      var avgString = "Data reveived from " + names + ", and the average temperature is "+ average +"*C";
+      io.emit(avgString); 
+    }
+    setInterval(function(){calc_avg()}, 1 * 2000);*/
   });
 });
