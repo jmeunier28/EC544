09fd4ccf523ecd51ca8b796b5ecad65e32c7e9a9
= <jmeunier@bu.edu>
2016-09-28 17:56:02 -0400

    New org

--- challengeTwo/app.js
+++ challengeTwo/app.js
@@ -1,41 +1,36 @@
+/*
+
+EC544 Group 4
+
+Challenge 2
+
+Visualize Temperature Data
+
+*/
+
+/* ----------- Declare Dependencies ------------ */
+
 var SerialPort = require("serialport");
-var app = require('express')();
+var express = require('express');
+app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var ON_DEATH = require('death');
+
 var mongo = require('mongodb');
 var monk = require('monk');
 var db = monk('localhost:27017/challengeTwo');
 
-app.use(function(req,res,next){
-    req.db = db;
-    next();
-});
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var portName = '/dev/cu.usbserial-AD01SSII',
+portConfig = {
+  baudRate: 9600,
+  parser: SerialPort.parsers.readline("\n")
+};
 
-var app = express();
+app.use(express.static(__dirname + '/public'));
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
+/* ------------ Error Handling ---------------*/
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -68,22 +63,23 @@ app.use(function(err, req, res, next) {
   });
 });
 
+/* -----Run App on LocalHost Port 3000----- */
 
-module.exports = app;
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
 
-var portName = process.argv[2],
-portConfig = {
-  baudRate: 9600,
-  parser: SerialPort.parsers.readline("\n")
-};
+/* ------------- App Logic ------------ */
 
 
 var sp;
 sp = new SerialPort.SerialPort(portName, portConfig);
 
-/*app.get('/', function(req, res){
-  res.sendfile('index.html');
-});*/
+app.get('/', function(req, res){
+  res.sendfile('./public/index.html');
+});
+
+//can also create other html page to render
 
 io.on('connection', function(socket){
   console.log('a user connected');
@@ -95,213 +91,135 @@ io.on('connection', function(socket){
   });
 });
 
-/*http.listen(3000, function(){
-  console.log('listening on *:3000');
-});*/
-var server = app.listen(3000, function () {
 
-   var host = server.address().address
-   var port = server.address().port
-
-   console.log("Example app listening at http://%s:%s", host, port)
-})
-
-//code purposely commented out will add back later
-/*var info_object = function(){
-  this.id;
-  this.info = [];
-}*/
-
-sp.on("open", function () {
-  console.log('open');
-
-  var tempArray = [0,0,0,0];
-  //var tempArray = [];
-  var names = []; //names will be assigned to each arduino 
-  var countArray = [0,0,0,0];
-  //var countArray = [];
-  var times = [];
-  var sensor_array = [];
-
-  sp.on('data', function(data) {
-    var flag = false;
-    var time = new Date();
-
-    times.push(time.getTime()); //add to time array
-    //console.log(data); // log incoming data from XBee
-    message = data.split(','); //separate data
-    //console.log(message);
-
-    //code purposely commented out... would give ability to scale to many devices
-
-    /*if (sensor_array.length === 0){
-
-        var whatup = new info_object();
-        whatup.id = message[0];
-        whatup.info.push(message[1]);
-        console.log(message[0]);
-        sensor_array.push(whatup);
-        console.log("Creating initial ID");
-
-    }
-
-    for(var i=0; i<sensor_array.length; i++){
-
-      if(sensor_array[i].id=== String(message[0])){
-        console.log(sensor_array[i].id);
-        sensor_array[i].info.push(message[1]);
-        flag = true;
-        console.log("I already have " + sensor_array[i].id + " and I'm adding to it");
-      }
-    }
-
-    if (flag===false){
-        console.log("Creating new id object...");
-        var heythere = new info_object();
-        heythere.id = message[0];
-        heythere.info.push(message[1]);
-        sensor_array.push(heythere);
-      }
+  sp.on("open", function (err) {
+    if (err) 
+      return console.log('Error opening port: ', err.message);
+    console.log('open');
+    
+    // INFO VARIABLES
+    var connectedNodes = [];
+    var buffer0;
+    var nodeInfo; 
+    var nodesArray = [[0,0,0]]; // remember that length is +1
+    var numNodes = 0;
+    
+    // Cleanup when termination signals are sent to process
+    ON_DEATH(function(signal, err) {
+      var death_msg = "Q";
+      sp.write(death_msg);
+      sp.close();   
+      console.log("\n\nSending reset signal to nodes.\n\n")
+      process.exit();
+    })
     
     
-    for (var i = 0; i < sensor_array.length-1;i++){
-      names.push(sensor_array[i]['id']);
-      tempArray.push(sensor_array[i]['info'].pop());
-    }
-    console.log("This is temp array " + tempArray);*/
+    // Check for and remove old nodes
+    var CheckNodes = function() {
+      var time = new Date();  
+      var nodeCount = nodesArray.length - 1;
+      for (var i = 1; i <= numNodes; i++) {
+        var t = time.getTime();
+        var timeDiff = t - nodesArray[i][2]; 
+        if (timeDiff > 2200) {
+            // remove node's subarray
+            console.log("Removing stale node " + nodesArray[i][0])
+            nodesArray.splice(i, 1);
+            numNodes--;
+            //console.log("numNodes: " + numNodes);
+        }
+      } }
+    setInterval(function(){ CheckNodes() }, 600); 
+    // 2100+300 (check) < 2500 (avg) > 2000 (xmit)
+    // looks like these times work  
 
-
-   if(message[0]==='A'){
-
-      names.push(message[0]);
-      tempArray[0]= parseInt(message[1]);
-      countArray[0]++;
-
-     //console.log(TempArray[0]);
-   }
-
-   if(message[0]==='B'){
-
-      names.push(message[0]);
-      tempArray[1] = parseInt(message[1]);
-      countArray[1]++;
-
-   }
-
-   if(message[0]==='C'){
-
-      names.push(message[0]);
-      tempArray[2] = parseInt(message[1]);
-      countArray[2]++;
-
-   }
-
-   if(message[0]==='D'){
-
-      names.push(message[0]);
-      tempArray[3] = parseInt(message[1]);
-      countArray[3]++;
-
-   }
-
-   // get all 4 entries and sum them together after the array 
-   // of times has 4 entries 
-
-  //var timeDiff = times[times.length - 1] - times[0];
-    var error = false;
-    var count = 0;
-  
-var calc_average = function(){
-
-   
-  if (tempArray.length === 4){
-
-    var counter = 0;
-    var sum = 0;
-    //console.log("counting average...");
-
-
-      for (var i = 0; i < tempArray.length;i++){
-        sum += tempArray[i]; //add each temperature together 
-        counter++;
+      var CalcAverage = function() {
+      var tempSum = 0;
+      var nodeCount = nodesArray.length - 1;
+      for (var i = 1; i <= nodeCount; i++) {
+        // Make sure value in temp index is a number
+        if (!isNaN(parseFloat(nodesArray[i][1]))) {
+          tempSum += parseFloat(nodesArray[i][1]);
+        }
       }
+      var tempAverage = tempSum / numNodes;
+      //console.log("nodeCount: " + nodeCount);
+      //console.log("tempSum: " + tempSum); 
+      if (!isNaN(tempAverage)) {
+        console.log("\nNode count: " + nodeCount + "   Average temperature: " + tempAverage.toFixed(2) + "F");
+        io.emit('chat message',tempAverage);       
+        for (var j = 1; j <= numNodes; j++) {
+          console.log("  " + ("     " + nodesArray[j][0]).slice(-7) + " : " + nodesArray[j][1] + "F")   
+        }
+      }
+      if (numNodes == 0) {
+        console.log("No nodes currently connected. Waiting for connection . . .")
+      } 
+     
     }
-
-    if (error){
-      var average = (sum/(counter - count)).toFixed(2);
-    }
-    else{
-      var average = (sum/counter).toFixed(2);
-    }
-
     
-  //console.log("Data reveived from " + names + ", and the average temperature is "+ average +"*C. ");
-
-      var avgString = average;
+    setInterval(function(){ CalcAverage() },2500); 
+    
+    
+    // listen for data, grab temp & time, populate array
+    sp.on('data', function(data) {
+      sp.flush();
+      var time = new Date();  
+      buffer0 = data.split('\n'); // array of data till newline
+      // Split again into either [#ID] or [#ID, Data] 
+      nodeInfo = buffer0[0].split('   ');
+      // Receiving good data ( ID or [ID, Data] )
+      if (nodeInfo[0][0] == "#") { // could check up here for && no temp data too 
+        
+        // New node
+        if (connectedNodes.indexOf(nodeInfo[0]) == -1) {
+          CheckNodes();
+          var dumNum = numNodes + 1;
+          console.log("\n (" + dumNum + ") NEW CONNECTION: Sending handshake confirmation to: " + nodeInfo[0])
+          connectedNodes.push(nodeInfo[0]);     
+          sp.write(nodeInfo[0]);
+        }
+        
+        // Preconnected node
+        else if (connectedNodes.indexOf(nodeInfo[0]) != -1) {
+          currentTime = time.getTime();
+          //console.log("  Connected node: " + nodeInfo[0] + "   " + nodeInfo[1]);
       
-      console.log(avgString);
-      io.emit('chat message',avgString);
-    }
+          // If node has temp data: [ID, Temp]
+          if (typeof nodeInfo[1] != 'undefined') {
+        
+            // Go through Nx3 array, see if this node ID is in nodesArray
+            var foundAtIndex;
+            for (var i = 0; i < nodesArray.length; i++)  {
+              // If found ID
+              if (nodesArray[i].indexOf(nodeInfo[0]) != -1) {
+                foundAtIndex = i;
+                break;
+              } 
+            }
+            // Push [ID, Temp, Time] to nodesArray if ID doesn't exist within it
+            if (typeof foundAtIndex === 'undefined') {
+              numNodes++;
+              //console.log("added to nodesArray") // debug print statement
+              var idTempTime = [nodeInfo[0], nodeInfo[1], currentTime]; 
+              nodesArray.push(idTempTime);
+            } 
+            // Update Temp, Time if ID already exists in nodesArray
+            else {
+              nodesArray[foundAtIndex][1] = nodeInfo[1];
+              nodesArray[foundAtIndex][2] = currentTime;
+              //console.log("  updated time/temp") // debug print statement
+            }
+          }
+        } 
+      }
+      // Bad data coming in
+      else if (nodeInfo[0][0] != "#") {
+        // FOR DEBUGGING PURPOSES, DELETE LATER
+        console.log("Corrupted node data. Push reconnect button if not already connected.")
+      }
+    // return the calulated average 
+ 
+    }); // end data
 
-  setInterval(function(){ calc_average() },2000);
-
-
-  for (var i = 0; i < countArray.length;i++){
-    if (countArray[i]=== 0 ){
-      console.log("Error Sending Data From Sensor Number " + i);
-      var errmsg = "Error Sending Data From Sensor Number " + i + "...Correcting Average";
-      count++
-      //io.emit('chat message',errmsg);
-    }
-  }
-  if (count > 0){
-    error = true;
-    console.log("Adjusting Average...");
-  }
-    //code purposely commented out... calc temp avg of device ids that have been added
-    //dynamically... not functioning at this time
-    /*var counter = 0;
-    var sum = 0;
-    var calc_avg = function(){
-    //console.log("temp array is "+ tempArray);
-
-
-      for (var i = 0; i < tempArray.length;i++){
-      console.log("im computing the average");
-      sum += tempArray[i]; //add each temperature together 
-      console.log("this is the sum" + sum);
-      counter++; }
-
-      var average = (sum/counter).toFixed(2);
-      console.log("Data reveived from " + names + ", and the average temperature is "+ average +"*C. ");
-      var avgString = "Data reveived from " + names + ", and the average temperature is "+ average +"*C";
-      io.emit(avgString); 
-    }
-    setInterval(function(){calc_avg()}, 1 * 2000);*/
-  });
-});
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
-
-module.exports = app;
+  }); // end open
