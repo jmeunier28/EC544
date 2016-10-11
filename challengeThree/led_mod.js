2b866d69c7f8dfcd79adcf404e68a56727d26f5c
= <jmeunier@bu.edu>
2016-10-11 15:12:59 -0400

    final code

--- challengeThree/led_mod.js
+++ challengeThree/led_mod.js
@@ -26,7 +26,7 @@ app.use(express.static(__dirname + '/public'));
 
 /* ----- Configure Serial Port -------- */
 
-var portName = '/dev/cu.Bluetooth-Incoming-Port',
+var portName = '/dev/cu.usbserial-AD01SSII',
 portConfig = {
 	baudRate: 9600,
 };
@@ -76,51 +76,93 @@ http.listen(3000, function(){
 var sp;
 sp = new SerialPort.SerialPort(portName, portConfig);
 sp.on("open", function (err) {	
+	var buffer;
+	str = "Connection Error Cant Read LED Status";
+	var red = false;
+	var blue = false;
+	var green = false;
+	var kill = false;
 
 	// Turn on
 	console.log("Sending LED on signal.");
 	console.log("Ctrl+C to quit and send LED off signal.");
 
 
-	io.on("connection",function(socket){
+		sp.on('data',function(data){ //grab data from XBee 
+			buffer = String(data).split('\n');
+			//console.log(buffer[0]);
+			if (buffer[0] === "Red is ON"){
+				red = true;
+				blue = false;
+				green = false;
+				kill = false;
+				str = buffer[0];
+			}
+			if (buffer[0] === "Green is ON"){
+				green = true;
+				blue = false;
+				red = false;
+				kill = false;
+				str = buffer[0];
+			}
+			if (buffer[0] === "Blue is ON"){
+				blue = true;
+				red = false;
+				green = false;
+				kill = false;
+				str = buffer[0];
+			}
+			if (buffer[0] === "LEDs are OFF"){
+				kill = true;
+				red = false;
+				green = false;
+				blue = false;
+				str = buffer[0];
+			}
+
+		}); //end data conn
+
+		io.on("connection",function(socket){
+		
 	  
-	  console.log('a user connected');
-	  
-	  socket.on('redbuttonPress',function(string){
-	  		console.log("Red Button Press");
-	  		var str = "You Turned ON The Red LED";
-	  		socket.emit('msg', str);
-			sp.write('1'); //red button pressed write 1 
-			}); //end red
+		console.log('a user connected');
 
-	  socket.on('greenbuttonPress',function(string){
-	  		console.log("Green Button Press");
-	  		var str = "You Turned ON The Green LED";
-	  		socket.emit('msg', str);
-			sp.write('2'); //green button pressed write 1 
-			}); // end green
+		  socket.on('redbuttonPress',function(string){
+		  		console.log("Red Button Press");
+		  		var str = "Red is ON";
+		  		socket.emit('msg', str);
+				sp.write('1'); //red button pressed write 1 
+				}); //end red
 
-	  socket.on('bluebuttonPress',function(string){
-	  		console.log("Blue Button Press");
-	  		var str = "You Turned ON The Blue LED";
-	  		socket.emit('msg', str);
-			sp.write('3'); //blue button pressed write 1 
-			}); // end blue
+		  socket.on('greenbuttonPress',function(string){
+		  		console.log("Green Button Press");
+		  		var str = "Green is ON";
+		  		socket.emit('msg', str);
+				sp.write('2'); //green button pressed write 1 
+				}); // end green
+
+		  socket.on('bluebuttonPress',function(string){
+		  		console.log("Blue Button Press");
+		  		var str = "Blue is ON";
+		  		socket.emit('msg', str);
+				sp.write('3'); //blue button pressed write 1 
+				}); // end blue
 
 
-	  socket.on('killbuttonPress',function(string){
-	  		console.log("Kill Button Press");
-	  		var str = "You Turned Off The LED";
-	  		socket.emit('msg', str);
-			// Turn off on program exit
-			ON_DEATH(function(signal, err) {
-				var death_msg = 'Q';
-				sp.write(death_msg);
-				sp.close();   
-				console.log("\n\nSending reset signal to nodes.\n\n")
-				process.exit();
-			}); //end ondeath 
-		}); // end kill button
+		  socket.on('killbuttonPress',function(string){
+		  		console.log("Kill Button Press");
+		  		var str = "LED is OFF";
+		  		socket.emit('msg', str);
+		  		sp.write('Q');
+				// Turn off on program exit
+				/*ON_DEATH(function(signal, err) {
+					var death_msg = 'Q';
+					sp.write(death_msg);
+					sp.close();   
+					console.log("\n\nSending reset signal to nodes.\n\n")
+					process.exit();
+				}); //end ondeath */
+			}); // end kill button
 			
 	}); //end io conn
 }); // end open serial port
