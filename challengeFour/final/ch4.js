91b9acede2f8c9c7319d7d70dd05d090aee478d1
Dami Or <dao@bu.edu>
2016-10-19 01:17:32 -0400

    connect to mongo

--- challengeFour/final/ch4.js
+++ challengeFour/final/ch4.js
@@ -23,10 +23,8 @@ var dev1, dev2, dev3, dev4;
 
 var datetime = new Date();
 
-//const deviceId = this.get('deviceId');
 const token = process.env.particle_token;
 
-//console.log(deviceId);
 
 
 /*----------- Connecting to Mongo --------------- */
@@ -36,26 +34,14 @@ var assert = require('assert');
 var ObjectId = require('mongodb').ObjectID;
 var url = 'mongodb://localhost:27017/ch4_Test';
 
-MongoClient.connect(url, function (err, db) {
-  if (err) {
-    console.log('Unable to connect to the mongoDB server. Error:', err);
-  } else {
-    
-    console.log('Connection established to', url);
-
-
-    //Close connection
-    db.close();
-  }
-});
 
 particle.login({
     username: process.env.particle_username, 
     password: process.env.particle_password 
 }).then(
   function(data){
-    console.log('API call completed on promise resolve');
-    //setTimeout(getDevs, 2000);
+    //console.log('API call completed on promise resolve');
+    setTimeout(getDevs, 2000);
   },
   function(err) {
     console.log('API call completed on promise fail: ', err);
@@ -70,54 +56,49 @@ var getDevs = particle.listDevices({ auth: token }).then(
       if(getInfo.connected){
         var devName = getInfo.name;
 
-        // console.log('Dev ID: ', getInfo.id)
-        // console.log('Dev ID: ', devName)
+         console.log('Dev ID: ', getInfo.id)
+         console.log('Dev ID: ', devName)
 
-         particle.getVariable({ deviceId: getInfo.id, name: 'tempFer', auth: token }).then(function(data) {
-  var currentTemp = data.body.result;
-  console.log('Dev ID: ', devName)
-  console.log('Temp: ', currentTemp)
-  console.log('\n ')
+         particle.getEventStream({ name: 'indTemp', auth: token}).then(function(stream) {
+  stream.on('event', function(data) {
+   
+   var curTmp = data.data;
+   //console.log(curTmp);
 
-}, function(err) {
-  console.log('An error occurred while getting attrs:', err);
+   MongoClient.connect(url, function (err, db) {
+  if (err) {
+    console.log('Unable to connect to the mongoDB server. Error:', err);
+  } else {
+    
+    //console.log('Connection established to: ', url);
+
+    var document = {"Time": datetime, "Temp": curTmp};
+    //console.log('Doc Format: ', document)
+
+db.collection(devName).insert(document, function(err, records) {
+            if (err) throw err;
+            //console.log("Record added as "+records[0]._id);
+            console.log(devName,": inserted into collection");
+         });
+
+//     Close connection
+//db.close();
+  }
 });
 
+   // console.log("Event: " + data.name + "   " + data.data);
+  });
+});
+
+
+
       }
-      //var testName = devices.body;
-      //console.log('TEST: ', testName);
-
-    })
-    //console.log('TEST: ', hello);
-
-    //console.log('Devices: ', devices);
-     // dev1 = devices.body[0].name;
-     // dev2 = devices.body[1].name;
-     // dev3 = devices.body[2].name;
-     // dev4 = devices.body[3].name;
-    // console.log('ID 1:', dev1);
-    // console.log('ID 2:', dev2);
-    // console.log('ID 3:', dev3);
-    // console.log('ID 4:', dev4);
-
-
-//     particle.getVariable({ deviceId: hello, name: 'tempFer', auth: token }).then(function(data) {
-//   console.log('Device variable retrieved successfully:', data);
-
-// }, function(err) {
-//   console.log('An error occurred while getting attrs:', err);
-// });
-    setTimeout(devicesPr, 2000);
+      });
+    
+    setTimeout(getDevs, 2000);
   },
   function(err) {
     console.log('List devices call failed: ', err);
   }
   
 );
-
-
-
-
-//Particle.variable("tempFer", &temp_K, DOUBLE);
-
-
