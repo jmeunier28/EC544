3badf4b9cdf008bbeb53b0db839301d41cc9ee57
Kiran Vishal Thanjavur Bhaaskar <vishal94@bu.edu>
2016-10-20 03:17:08 -0400

    Updated .ino code

--- challengeFour/KV_Phototemp/KV_Phototemp.ino
+++ challengeFour/KV_Phototemp/KV_Phototemp.ino
@@ -1,12 +1,43 @@
+// This #include statement was automatically added by the Particle IDE.
+#include "Adafruit_DHT/Adafruit_DHT.h"
+
+// This #include statement was automatically added by the Particle IDE.
+#include "ThingSpeak/ThingSpeak.h"
+
 
 #include <math.h>
+
+TCPClient client;
+/*
+VERY ACCURATE TEMPERATURE READINGS
+----------------------------------
+Better voltage reading:
+  Used A4 pin (less jumpy)
+  Got two measurements each pull, discarded the first
+Better R_therm reading:
+  Measured V_source accurately
+  measured R1 accurately
+Better temperature reading:
+  Used accurate temperature sensor to get 3 data points
+  Solved resulting system of equations for Steinhart-Hart coefficients
+  Base conversion (needed base-2 instead of base-10)
+  (temp slightly rising at start due to current self-heating unavoidable with thermistor)
+*/
 
 // Global variables
 int AnalogPin = A4; // has much less jumpy readings than A0 (maybe A0 slightly damage from overuse)
 float V_source = 4.90; // measured
 //String randID = "";
 //int handshake = 0;
+unsigned int myChannelNumber = 172815;
+const char * myWriteAPIKey = "KD2E4ZEG5996CJNY";
+const char * name = "Dev1";
 
+int led = D7;
+int trans = 0;
+int t = 0;
+double temp_F;
+double T1;
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // Calculate thermistor voltage
@@ -44,8 +75,6 @@ float ConvertTemp(float temp_K, char T) {
     return (temp_K * 1.8 - 459.67);
 }
 
-int led = D7;
-int trans = 0;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -54,7 +83,10 @@ void setup() {
  
  pinMode(led, OUTPUT);
  Particle.function("transmission", ToggleTrans);
+ //we can access the value of the Temp from the cloud
+  Particle.variable("tempFer", temp_F);
  digitalWrite(led, LOW);
+// Particle.publish("PowerON","Dev1",60,PRIVATE);
  
 }
 
@@ -68,14 +100,26 @@ void loop()
     float R_therm = Thermistor(V_therm);
     float temp_K = Temperature(R_therm);
     float temp_C = ConvertTemp(temp_K, 'C');
-    float temp_F = ConvertTemp(temp_K, 'F');
+   // float temp_F = ConvertTemp(temp_K, 'F');
+    temp_F = ConvertTemp(temp_K, 'F');
     //Spark.publish("VOLTAGE", String(V_therm));
     //Spark.publish("Raw_ADC", String(RawADC));
     //Spark.publish("R_Therm", String(R_therm));
     //Spark.publish("Kelvin", String(temp_K) );
-    //Spark.publish("Temperature (°C)", String(temp_C) + " °C");  // return degrees C
-    Spark.publish("Temperature (°F)", String(temp_F) + " °F");   // return degrees F
+    //Spark.publish("Temperature (°C)", String(temp_C));  // return degrees C
+     T1 = temp_F;
+   Spark.publish("Temperature (°F)", String(temp_F));   // return degrees F
  //thermister_temp(analogRead(4));
+  //t = t + 1;
+ // T1 = temp_F;
+ // T1 = String(temp_F);
+  //string(t);
+  //ThingSpeak.setField(1,t);
+  //ThingSpeak.setField(2,name);
+ // ThingSpeak.setField(2,temp_F);
+  
+//  ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);  
+  Particle.publish("temperature",String(temp_F),PRIVATE);
     }
     else{
         delay(500);
