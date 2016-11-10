1cabbd12b5b2fe5202557618671c2e564a1c9968
Dami Or <dao@bu.edu>
2016-11-10 16:40:51 -0500

    format

--- challengeFour/final/ch4.js
+++ challengeFour/final/ch4.js
@@ -47,7 +47,7 @@ var devCount = 0;
 
 function getFormattedDate() {
     var date = new Date();
-    var str = date.getFullYear() + ":" + (date.getMonth() + 1) + ":" + date.getDate() + ":" +  date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+    var str = date.getFullYear() + ":" + (date.getMonth() + 1) + ":" + date.getDate() + ":" + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
     return str;
 }
 
