df2dcf8a1c1998ec4464a818d9b8c43f1ef77dbb
austinmw <austinmw@bu.edu>
2016-11-10 01:05:34 -0500

    update
    training points code

--- ChallengeSix/rssiServer.js
+++ ChallengeSix/rssiServer.js
@@ -1,6 +1,8 @@
-/* Run this code many times at different locations 
-setting argv[3] to cluster number to obtain multiple 
-training points for each cluster */
+/* Produces 1 training point in ~10 seconds
+Run this code 5-10 times at unique locations within each cluster.
+Set argv[2] to the cluster number for current training point.
+When finished, copy produced .csv file into new .xlsx file for use with matlab classifier.
+*/
 
 // requires
 var SerialPort = require("serialport");
@@ -8,27 +10,21 @@ var app = require('express')();
 var xbee_api = require('xbee-api');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var math = require('math');
-var Excel = require('exceljs');
-var workbook = new Excel.Workbook();
-workbook.creator = 'Me';
-workbook.lastModifiedBy = 'Me';
-workbook.created = new Date();
-workbook.modified = new Date();
-var sheet = workbook.addWorksheet('rssi');
-file = 'rssi_values copy.xlsx'
+var fs = require('fs');
 
 // xbee
 var C = xbee_api.constants;
 var XBeeAPI = new xbee_api.XBeeAPI({
 api_mode: 2
 });
-var portName = process.argv[2];
+var port = '/dev/cu.usbserial-AD01SSIK'
+var portName = port
 
 // cluster/bin
-var binNum = process.argv[3];
+var binNum = process.argv[2];
+var numSamples = 3; // 10
 console.log('\nTraining point for Bin #', binNum)
-console.log('Running till each beacon gets at least 10 rssi values to average.\n')
+console.log(' Running till each beacon gets at least', numSamples ,'rssi values to average.')
 
 // variables
 var sampleDelay = 2000;
@@ -62,94 +58,74 @@ sp.write(XBeeAPI.buildFrame(RSSIRequestPacket));
 
 // loop
 sp.on("open", function () {
-
 	// start main function, set interval
 	requestRSSI();
 	setInterval(requestRSSI, sampleDelay);
-	});
-
 	// input
 	XBeeAPI.on("frame_object", function(frame) {
-	if (frame.type == 144){
-
-	// print input
-	//console.log(frame);
-	console.log("Beacon ID: " + frame.data[1] + ", RSSI: " + (frame.data[0]));
-
-	// split input, sample number
-	var beaconID = frame.data[1];
-	var rssiVal = frame.data[0];
-	var numSamples = 3; // 10
-
-
-	//	if ((r1.length >= numSamples) && (r2.length >= numSamples) && 
-	//	    (r3.length >= numSamples) && (r4.length >= numSamples)){
-			
-		if (r4.length >= numSamples) { // for testing, use above with all 4 beacons
-			// Average each of the four beacons' values
-			var r1avg = 0;
-			for (var i = 0; i < r1.length; i++)
-				r1avg += r1[i];
-			r1avg /= r1.length;
-			finalPoints.push(r1avg);
-			var r2avg = 0;
-			for (var i = 0; i < r2.length; i++)
-				r2avg += r2[i];
-			r2avg /= r2.length;
-			finalPoints.push(r2avg);
-			var r3avg = 0;
-			for (var i = 0; i < r3.length; i++)
-				r3avg += r3[i];
-			r3avg /= r3.length;
-			finalPoints.push(r3avg);
-			var r4avg = 0;
-			for (var i = 0; i < r4.length; i++)
-				r4avg += r4[i];
-			r4avg /= r4.length;
-			finalPoints.push(r4avg);
-			// add cluster number to end for matlab code
-			finalPoints.push(binNum);
-			// print		
-			console.log('finalPoints: ', finalPoints)
-
-
-
-// write to excel
-/* 
-This is the last part I need to complete, can't seem to
-append a row to the .xlsx file which matlab is going to use 
-just need to append the array finalPoints as a row, but having difficulties		
-*/	
-			
-			var row_test = {rssi1: 1, rssi2: 2}
-		
-			sheet.addRow(row_test)
-			workbook.xlsx.writeFile(file)
-			.then(function() {
-				console.log('Array added and file saved.')
-			});
-	
-	
-			// exit program, rerun at new location
-			process.exit();
+		if (frame.type == 144){
+			// split input, sample number
+			var beaconID = frame.data[1];
+			var rssiVal = frame.data[0];
+			// check if finished data collection	
+			if ((r1.length >= numSamples) && (r2.length >= numSamples) && 
+			    (r3.length >= numSamples) && (r4.length >= numSamples)){
+			//if (r4.length >= numSamples) { // for testing, use above with all 4 beacons
+				// Average each of the four beacons' values
+				var r1avg = 0;
+				for (var i = 0; i < r1.length; i++)
+					r1avg += r1[i];
+				r1avg /= r1.length;
+				finalPoints.push(r1avg);
+				var r2avg = 0;
+				for (var i = 0; i < r2.length; i++)
+					r2avg += r2[i];
+				r2avg /= r2.length;
+				finalPoints.push(r2avg);
+				var r3avg = 0;
+				for (var i = 0; i < r3.length; i++)
+					r3avg += r3[i];
+				r3avg /= r3.length;
+				finalPoints.push(r3avg);
+				var r4avg = 0;
+				for (var i = 0; i < r4.length; i++)
+					r4avg += r4[i];
+				r4avg /= r4.length;
+				finalPoints.push(r4avg);
+				// add cluster number to end for matlab code
+				finalPoints.push(binNum);
+				// print		
+				console.log('  Training point values and bin: ', finalPoints)
+				// write to file and exit
+				fs.appendFile('rssi_values.csv', finalPoints + '\n', 'utf8', function(err) {
+					if (err) {
+						console.log('error occurred writing to file.\n')
+					}
+					else {
+						console.log('  Appended new training point to file. Ready to rerun.\n')
+						process.exit();
+					}
+				});
+				
+			}
+			// populate beacon arrays up to sample number, error check, print data and totals
+			else if ((rssiVal != 0) && (rssiVal != 255)){
+				console.log("  Beacon ID: " + beaconID + ", RSSI: " + rssiVal);
+				console.log('   Pushing value to beacon #', beaconID)
+				if (beaconID == 1){
+				r1.push(rssiVal);
+				}
+				else if(beaconID == 2){
+				r2.push(rssiVal);
+				}
+				else if(beaconID == 3){
+				r3.push(rssiVal);
+				}
+				else if(beaconID == 4){
+				r4.push(rssiVal);
+				}
+				console.log("    Totals::  r1:", r1.length, "  r2:", r2.length, "  r3:", r3.length, "  r4:", r4.length)
+			}
 		}
-
-		// populate beacon arrays up to sample number, error check, print totals
-		else if ((rssiVal != 0) && (rssiVal != 255)){
-			console.log('  pushing value to beacon #', beaconID)
-			if (beaconID == 1){
-			r1.push(rssiVal);
-			}
-			else if(beaconID == 2){
-			r2.push(rssiVal);
-			}
-			else if(beaconID == 3){
-			r3.push(rssiVal);
-			}
-			else if(beaconID == 4){
-			r4.push(rssiVal);
-			}
-			console.log("   totals::  r1:", r1.length, "  r2:", r2.length, "  r3:", r3.length, "  r4:", r4.length)
-		}
-	}
+	});
 });
