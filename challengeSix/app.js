fa73f4b2d120418ad328df0339bd0d43753e12af
= <jmeunier@bu.edu>
2016-11-10 22:56:12 -0500

    updates to server code to cancel py process

--- challengeSix/app.js
+++ challengeSix/app.js
@@ -1,8 +1,10 @@
 /*
+
 EC544 group 4
-Challenge Five 
+Challenge Six 
 Read values from XBee
-Drive Car in Straight Line
+And locate object on map
+
 */
 
 /* -------- Declare Dependices --------- */
@@ -70,7 +72,7 @@ var XBeeAPI = new xbee_api.XBeeAPI({
   api_mode: 2
 });
 
-var portName = '/dev/cu.usbserial-AD01SSII',
+var portName = '/dev/cu.usbserial-DA01LOA2';
 var sampleDelay = 3000;
 
 
@@ -97,51 +99,144 @@ var requestRSSI = function(){
 
 /* -------- App Logic ----------*/
 
+var numSamples = 5;
+
+console.log(' Running till each beacon gets at least', numSamples ,'rssi values to average.')
+
+// variables
+var sampleDelay = 2000;
+var r1 = [];
+var r2 = [];
+var r3 = [];
+var r4 = [];
+var finalPoints = [];
+r1Arr = [];
+r2Arr = [];
+r3Arr = [];
+r4Arr = [];
+options = {
+	pythonOptions: ['-W', 'ignore'], //for supressing warnings
+	scriptPath: '/Users/jmeunier28/Desktop/EC544/challenges/EC544/challengeSix',
+	args: [44.2, 59.1, 88.3, 100.9]
+};
+
+
 sp.on("open", function () {
-  console.log('open');
-  requestRSSI();
-  setInterval(requestRSSI, sampleDelay);
-});
+	// start main function, set interval
+	requestRSSI();
+	setInterval(requestRSSI, sampleDelay);
+	// input
+	XBeeAPI.on("frame_object", function(frame) {
+		if (frame.type == 144){
+			// split input, sample number
+			var beaconID = frame.data[1];
+			var rssiVal = frame.data[0];
+			// check if finished data collection	
+			if ((r1.length >= numSamples) && (r2.length >= numSamples) && 
+			    (r3.length >= numSamples) && (r4.length >= numSamples))
+			{
+			//if (r4.length >= numSamples) { // for testing, use above with all 4 beacons
+				// Average each of the four beacons' values
+				var r1avg = 0;
+				for (var i = 0; i < r1.length; i++)
+					r1avg += r1[i];
+				r1avg /= r1.length;
+				finalPoints.push(r1avg);
+				r1Arr.push(r1avg);
+				var r2avg = 0;
+				for (var i = 0; i < r2.length; i++)
+					r2avg += r2[i];
+				r2avg /= r2.length;
+				finalPoints.push(r2avg);
+				r2Arr.push(r2avg);
+				var r3avg = 0;
+				for (var i = 0; i < r3.length; i++)
+					r3avg += r3[i];
+				r3avg /= r3.length;
+				finalPoints.push(r3avg);
+				r3Arr.push(r3avg);
+				var r4avg = 0;
+				for (var i = 0; i < r4.length; i++)
+					r4avg += r4[i];
+				r4avg /= r4.length;
+				finalPoints.push(r4avg);
+				r4Arr.push(r4avg);
+				// add cluster number to end for matlab code
+				//finalPoints.push(binNum);
+				// print		
+				console.log('Training point values and bin: ' + r1Arr[0] + r2Arr[0] + r3Arr[0] + r4Arr[0])
+				
+			}
+			// populate beacon arrays up to sample number, error check, print data and totals
+			else if ((rssiVal != 0) && (rssiVal != 255))
+			{
+				//console.log("  Beacon ID: " + beaconID + ", RSSI: " + rssiVal);
+				//console.log('   Pushing value to beacon #', beaconID)
+				if (beaconID == 1){
+				r1.push(rssiVal);
+				}
+				else if(beaconID == 2){
+				r2.push(rssiVal);
+				}
+				else if(beaconID == 3){
+				r3.push(rssiVal);
+				}
+				else if(beaconID == 4){
+				r4.push(rssiVal);
+				}
+				//console.log("Totals::  r1:", r1.length, "  r2:", r2.length, "  r3:", r3.length, "  r4:", r4.length);
+			}
 
-XBeeAPI.on("frame_object", function(frame) {
-  if (frame.type == 144){
-    console.log("Beacon ID: " + frame.data[1] + ", RSSI: " + (frame.data[0]));
-  }
-});
+		}
 
 
-io.on("connection",function(socket){
-	
-  
-	console.log('a user connected');
-	var data_point = [12, 22, 3, 12.6];
-	//var train = 1;
+		setInterval(function()
+		{
+			options['args'][0] = r1Arr.pop();
+			options['args'][1] = r2Arr.pop();
+			options['args'][2] = r3Arr.pop();
+			options['args'][3] = r4Arr.pop();
+			for (var i = 0; i < options['args'].length; i++){
+				if (options['args'][i] == 'undefined'){
+					options['args'][i] = 50; // randomly reassingn so program keeps running
+				}
+			}
+			console.log(options['args']);
+			console.log("In interval function")
+			console.log("options are: " + options['args']);
 
-	var options = {
-		pythonOptions: ['-W', 'ignore'], //for supressing warnings
-		scriptPath: '/Users/jmeunier28/Desktop/EC544/challenges/EC544/challengeSix',
-		args: [data_point[0], data_point[1], data_point[2], data_point[3]]//, train]
-		};
+			pyshell = new PythonShell('classifier.py', options);
+			// // pyshell.end(function (err) {
+			// // 	if (err) throw err;
+			// // 	console.log('finished');
+			// // });
+			pyshell.on('message', function (message) 
+			{
+				console.log("Calling python classifier");
+				console.log("\nPoint is: \n" + options['args']);
+				//if (err) throw err;
+				// results is an array consisting of messages collected during execution
+				//console.log('results: %j', results);
+				console.log("\nI think i'm in bin: " + message);
+				//socket.emit('left', results);
+			}); // end python call 
 
-	PythonShell.run('classifier.py', options, function (err, results) {
-		if (err) throw err;
-		// results is an array consisting of messages collected during execution
-		console.log('results: %j', results);
-		socket.emit('left', results);
-		});
+			pyshell.end(function (err) {
+				if (err) throw err;
+				console.log('finished');
+			});
 
-	// pyshell.on('message', function (message) {
-	// // received a message sent from the Python script (a simple "print" statement) 
-	// console.log(message);
-	// //var update = message;
-	// socket.emit('update', message);
+		}, 1*10000); // call every 5 seconds
 
-	// // end the input stream and allow the process to exit 
-	// pyshell.end(function (err) {
-
-	// if (err) throw err;
-	//     console.log('finished');
-	//   });
 		
+		io.on("connection",function(socket){
+  
+			console.log('a user connected');
+
 			
-}); //end io conn
+		}); //end io conn
+	}); // end Xbee conn
+}); // end serial port conn
+
+
+
--- challengeSix/classifier.py
+++ challengeSix/classifier.py
@@ -16,16 +16,19 @@ parser.add_argument("b1", help="signal Beacon one", type = float)
 parser.add_argument("b2", help="signal Beacon two", type = float)
 parser.add_argument("b3", help="signal Beacon three", type = float)
 parser.add_argument("b4", help="signal Beacon four", type = float)
-#parser.add_argument("train", help="signal to know if we need to train or not", type = int)
 args = vars(parser.parse_args())
 
 b1 = args.get("b1",None)
 b2 = args.get("b2",None)
 b3 = args.get("b3",None)
 b4 = args.get("b4",None)
-#train = args.get("train", None)
 
-nbrs = KNeighborsClassifier(n_neighbors=4, algorithm='auto')
+print("\nB1: "), print(b1)
+print("\nB2: "), print(b2)
+print("\nB3: "), print(b3)
+print("\nB4: "), print(b4)
+
+nbrs = KNeighborsClassifier(n_neighbors=10, algorithm='auto') # based off of 3 nearest neighbors
 
 class FindPoint:
 
@@ -54,21 +57,34 @@ class FindPoint:
 
 
 find = FindPoint()
-find.trainMe('train.csv','sample_rssi_values.csv')
-print(int(find.findBin(b1,b2,b3,b4)))
+find.trainMe('train2.csv','sample_rssi_values.csv')
+print(find.findBin(b1,b2,b3,b4))
 
-
-
-# # test this
+# train = pd.read_csv('train.csv')
+# test = pd.read_csv('test.csv')
+# train.head()
+# col = ['Beacon 1', 'Beacon 2','Beacon 3', 'Beacon 4']
+# col2 = ['Bin']
+# trainArr = train.as_matrix(col)
+# trainOut = train.as_matrix(col2)
+# testArr = test.as_matrix(col)
+# testOut = test.as_matrix(col2)
+# nbrs = KNeighborsClassifier(n_neighbors=10, algorithm='auto')
+# nbrs.fit(trainArr, trainOut) #fit the data
+# point = np.array([b1,b2,b3,b4])
+# point = point.reshape(1, -1)
+# bin = nbrs.predict(point)
+# print(bin)
+# location = nbrs.predict(testArr)
 
 # correct = 0.0
 
 # correct = 0.0
 # for i in range(len(location)):
-# 	print("\nActuatl Bin: ")
-# 	print(trainOut[i][0])
-# 	print(" Predicted Bin: ")
-# 	print(testOut[i][0])
+# 	# print("\nActuatl Bin: ")
+# 	# print(trainOut[i][0])
+# 	# print(" Predicted Bin: ")
+# 	#print(testOut[i][0])
 
 # 	if testOut[i][0] == location[i]: 
 # 		correct += 1
